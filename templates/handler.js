e0292732e80a58ab98bc30cfd245c1811c17d51f
pragnagopa <pgopa@microsoft.com>
2017-01-23 11:43:35 -0800

    Update Yml file with handler

---
+++ templates/handler.js
@@ -0,0 +1,22 @@
+module.exports.hello = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+    }
+    else {
+        res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done(null, res);
+};
+
+module.exports.helloQueue = function (context, myQueueItem) {
+    context.log('JavaScript queue trigger function processed work item', myQueueItem);
+    context.done();
+};
